6cd34d47a4d21b8a7ae6419f682b384b26c596c6
Akshaya <akshaya.23bcy10304@vitbhopal.ac.in>
2026-03-30 14:17:53 +0530

    organised repo

---
+++ Programming_phases/stl.cpp/print_vector.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector<int> a = {1,2,3,4,5};
+     for (int i=0; i<a.size(); i++) {
+        cout << a[i] << " ";
+     }
+
+    
+}
