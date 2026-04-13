731cbff8df8778a6085c09747c6d450536a68109
Akshaya <akshaya.23bcy10304@vitbhopal.ac.in>
2026-04-13 23:45:23 +0530

    queue q

--- Programming_phases/stl.cpp/firstn_bnqueue.cpp
+++ Programming_phases/stl.cpp/firstn_bnqueue.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-int main () {
-    
+
+int main() {
+    int n;
+    cin >> n;
+
+    queue<string> q;
+
+    q.push("1");
+
+    for (int i = 0; i < n; i++) {
+        string curr = q.front();
+        q.pop();
+
+        cout << curr << " ";
+
+        q.push(curr + "0");
+        q.push(curr + "1");
+    }
 }
