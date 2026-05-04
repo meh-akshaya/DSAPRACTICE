62b8d5655565dd91d87fe104e17732232f97bc51
Akshaya <akshaya.23bcy10304@vitbhopal.ac.in>
2026-05-04 23:39:16 +0530

    array

--- Programming_phases/array.cpp/subarraysumequalsk_array.cpp
+++ Programming_phases/array.cpp/subarraysumequalsk_array.cpp
@@ -2,13 +2,37 @@
 #include <vector>
 #include <map>
 using namespace std;
+
 int main() {
-    int N;
-    cin >> N;
-    vector <int> v(N);
-    vector <int> vsum;
-    for (int i=0; i<N; i++){
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
     }
-    
-    
+
+    int k;
+    cin >> k;
+
+    map<int, int> m;
+    m[0] = 1;  // important
+
+    int prefix = 0;
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        prefix += v[i];
+
+        int needed = prefix - k;
+
+        if (m.find(needed) != m.end()) {
+            count += m[needed];
+        }
+
+        m[prefix]++;
+    }
+
+    cout << count;
 }
