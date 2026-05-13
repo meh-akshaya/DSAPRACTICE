616716c6e4f71ceb85b9891ca02849aaeaf17a20
Akshaya <akshaya.23bcy10304@vitbhopal.ac.in>
2026-05-14 01:10:00 +0530

    two pointer

--- Programming_phases/array.cpp/3sum_array.cpp
+++ Programming_phases/array.cpp/3sum_array.cpp
@@ -11,8 +11,22 @@ int main() {
     }
     sort(v.begin(),v.end());
     for (int i=0; i<n; i++) {
-        
-    }
-    
+        int p = -v[i];
+        int j = i+1;
+        int k = n-1;
+        while (j<k) {
+            if (v[j]+v[k]==p) {
+                cout << "YES" << endl;
+                return 0;
+            }
+            else if (v[j]+v[k]>p) {
+                k--;
+            }
+            else {
+                j++;
+            }
+        }
 
+    }
+    cout << "NO" << endl;
 }
